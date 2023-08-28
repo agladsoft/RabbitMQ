6d796c8bfc0df1848de4c2fb974a42ca865a7dd9
timur <timurka.zavyalov@mail.ru>
2023-08-28 18:00:44 +0300

    add: added update deal and cache

--- scripts/data_core_client.py
+++ scripts/data_core_client.py
@@ -1,9 +1,7 @@
 import sys
 import time
-
-from tinydb import Query
-
 from app_logger import *
+from tinydb import Query
 from receive import Receive
 from typing import List, Union
 from tinydb.table import Document
@@ -17,7 +15,7 @@ logger: logging.getLogger = get_logger(os.path.basename(__file__).replace(".py",
 date_formats: tuple = ("%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S%z")
 
 
-class ClickHouse(Receive):
+class DataCoreClient(Receive):
     def __init__(self):
         super().__init__()
         self.client: Client = self.connect_to_db()
@@ -39,7 +37,7 @@ class ClickHouse(Receive):
             sys.exit(1)
         return client
 
-    def count_number_loaded_rows(self) -> None:
+    def count_number_loaded_rows(self) -> List[Document]:
         """
         Counting the number of rows to update transaction data.
         :return:
@@ -48,12 +46,19 @@ class ClickHouse(Receive):
         while all_data_cache[-1]["len_rows"] != self.client.query(
                 f"SELECT count(*) FROM datacore_freight WHERE original_file_parsed_on='{all_data_cache[-1]['file_name']}'"
         ).result_rows[0][0]:
+            logger.info("The data has not yet been uploaded to the database")
             time.sleep(10)
-        self.update_status(all_data_cache[-1], all_data_cache)
-        self.delete_data_from_cache_by_date()
+        return all_data_cache
 
     @staticmethod
     def set_nested(path: list, val: Union[bool, str], condition: str):
+        """
+        Deleting nested data from the cache.
+        :param path: Nested data path.
+        :param val: The value to replace the old one.
+        :param condition: Field from json (the current field is uuid).
+        :return:
+        """
         def transform(doc):
             list_current = doc
             for key in path[:-1]:
@@ -66,23 +71,23 @@ class ClickHouse(Receive):
         return transform
 
     @staticmethod
-    def change_values(is_obsolete, row, date_now):
+    def change_values(is_obsolete: bool, row: dict, date_now: str) -> None:
         """
-
-        :param is_obsolete:
-        :param row:
-        :param date_now:
+        Changing the data in the dictionary for writing to json and then uploading to the database.
+        :param is_obsolete: Setting the value of is_obsolete.
+        :param row: Dictionary of data.
+        :param date_now: write in json-file datetime now.
         :return:
         """
         row["is_obsolete"] = is_obsolete
         row['is_obsolete_date'] = date_now
 
-    def write_updated_data(self, query: str, data_cache: Union[Document, list], is_obsolete: bool):
+    def write_updated_data(self, query: str, data_cache: Union[Document, list], is_obsolete: bool) -> None:
         """
-
-        :param query:
-        :param data_cache:
-        :param is_obsolete:
+        Writing updated data to a json-file.
+        :param query: ClickHouse Client.
+        :param data_cache: Data from cache.
+        :param is_obsolete: Setting the value of is_obsolete.
         :return:
         """
         data_db: Sequence = self.client.query(query).result_rows
@@ -102,19 +107,25 @@ class ClickHouse(Receive):
                             self.write_to_json(row, index, dir_name="update")
         self.update_cache(data_db, is_obsolete, date_now)
 
-    def update_cache(self, data_db: Sequence, is_obsolete: bool, date_now: str):
-        """
-
-        :param data_db:
-        :param is_obsolete:
-        :param date_now:
-        :return:
-        """
-        for row in data_db:
-            self.db.update(self.set_nested(['data', 'is_obsolete'], is_obsolete, str(row[0])))
-            self.db.update(self.set_nested(['data', 'is_obsolete_date'], date_now, str(row[0])))
+    def update_cache(self, data_db: Sequence, is_obsolete: bool, date_now: str) -> None:
+            """
+            Updating data from the cache.
+            :param data_db: Data from the database.
+            :param is_obsolete: Setting the value of is_obsolete.
+            :param date_now: write in cache datetime now.
+            :return:
+            """
+            for row in data_db:
+                self.db.update(self.set_nested(['data', 'is_obsolete'], is_obsolete, str(row[0])))
+                self.db.update(self.set_nested(['data', 'is_obsolete_date'], date_now, str(row[0])))
 
     def remove_nested(self, path: list, condition: str):
+        """
+        Deleting nested data from the cache.
+        :param path: Nested data path.
+        :param condition: Current date minus the number of days.
+        :return:
+        """
         def transform(doc):
             list_current = doc
             for key in path[:-1]:
@@ -130,12 +141,12 @@ class ClickHouse(Receive):
 
         return transform
 
-    def delete_data_from_cache_by_date(self):
+    def delete_data_from_cache(self):
         """
-
+        Deleting data from the cache by date.
         :return:
         """
-        date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        date: str = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
         self.db.remove(self.remove_nested(['data', 'is_obsolete_date'], date))
 
     def update_status(self, data_cache: Document, all_data_cache) -> None:
@@ -146,7 +157,7 @@ class ClickHouse(Receive):
         self.write_updated_data(f"""
             SELECT *
             FROM datacore_freight
-            WHERE original_file_parsed_on='{data_cache['file_name']}'""", data_cache, is_obsolete=False)
+            WHERE original_file_parsed_on='{data_cache['file_name']}' AND is_obsolete is NULL""", data_cache, is_obsolete=False)
 
         group_list: list = list({dictionary['orderNumber']: dictionary for dictionary in data_cache["data"]}.values())
         for item in group_list:
@@ -164,7 +175,16 @@ class ClickHouse(Receive):
         self.client.query("DELETE FROM datacore_freight WHERE is_obsolete=true")
         logger.info("Successfully deleted old transaction data")
 
+    def main(self):
+        """
+        The main method that runs the basic functions.
+        :return:
+        """
+        all_data_cache_: List[Document] = self.count_number_loaded_rows()
+        self.update_status(all_data_cache_[-1], all_data_cache_)
+        self.delete_data_from_cache()
+
 
 if __name__ == "__main__":
-    clickhouse = ClickHouse()
-    clickhouse.count_number_loaded_rows()
+    data_core_client: DataCoreClient = DataCoreClient()
+    data_core_client.main()
