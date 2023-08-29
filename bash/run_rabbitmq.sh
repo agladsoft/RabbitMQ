e25a305ae6f54314aad1204392c5028ffbcff279
timur <timurka.zavyalov@mail.ru>
2023-08-29 11:11:21 +0300

    add: added update deal and cache

--- bash/run_rabbitmq.sh
+++ bash/run_rabbitmq.sh
@@ -4,7 +4,9 @@ do
   if [[ ${DEBUG}]]
 	  then
 	     python3 ${XL_IDP_ROOT_RABBITMQ}/scripts/receive.py
+	     python3 ${XL_IDP_ROOT_RABBITMQ}/scripts/data_core_client.py
 	else
 	  sleep 3600
 	  python3 ${XL_IDP_ROOT_RABBITMQ}/scripts/receive.py
+	  python3 ${XL_IDP_ROOT_RABBITMQ}/scripts/data_core_client.py
 done
