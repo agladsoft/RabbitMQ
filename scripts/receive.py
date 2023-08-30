e0396b11a9edf20638d1df2af172096641447f45
timur <timurka.zavyalov@mail.ru>
2023-08-30 16:08:51 +0300

    add: added update deal and cache

--- scripts/receive.py
+++ scripts/receive.py
@@ -62,7 +62,7 @@ class Receive(RabbitMq):
         :return:
         """
         self.logger: logging.getLogger = get_logger(os.path.basename(__file__).replace(".py", "_") + str(datetime.now().date()))
-        self.logger.info(f"callback for ch={ch}, method={method}, properties={properties} and body_message called")
+        self.logger.info(f"Callback for ch={ch}, method={method}, properties={properties} and body_message called")
         time.sleep(self.time_sleep)
         self.save_text_msg(body)
         self.read_json(body)
