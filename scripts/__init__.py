a0613170c0f8edcd023eb45a2aacf6eedc133c20
timur <agladsoft@gmail.com>
2025-02-18 15:30:04 +0300

    fix: changed queue name

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -6,73 +6,6 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
-LOG_FORMAT: str = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
-DATE_FTM: str = "%d/%B/%Y %H:%M:%S"
-
-TABLE_NAMES: dict = {
-    # Данные по DC
-    "СписокКонтрагентов":
-        "counterparties",
-    "ОтчетПоКонтролируемомуИНеконтролируемомуФрахту":
-        "datacore_freight",
-    "ОтчетНатуральныеПоказателиПоСделкамИСегментам":
-        "natural_indicators_by_contracts_segments",
-    "ОтчетПоПоручениям":
-        "orders_report",
-    "ОбщийОтчетПоАвтовывозу":
-        "auto_pickup_general_report",
-    "ВладельцыКонтейнеров":
-        "transport_units",
-    "СписокКоносаментов":
-        "consignments",
-    "РегистрСведенийПланПродаж":
-        "sales_plan",
-    "ОтчетПоНатуральнымНаОсновеОперацийПоФактическимДатам":
-        "natural_indicators_by_transaction_fact_date",
-    "РегистрСведенийКонтрагентыРазвитияПоЦФО":
-        "development_counterparty_by_department",
-    "ОтчетExportBookings":
-        "export_bookings",
-    "ОтчетImportBookings":
-        "import_bookings",
-    "ОтчетПоЗавершеннымПеретаркам":
-        "completed_repackages_report",
-    "ОтчетАвтовизиты":
-        "autovisits",
-    "ОтчетПоОбращениямВПЭО":
-        "accounting_documents_requests",
-    "ОтчетЕжедневнаяСводка":
-        "daily_summary",
-    "ОтчетПоЖДПеревозкамМаркетингПоОперациям":
-        "rzhd_by_operations_report",
-    "ОтчетПоМаржинальностиСделок":
-        "orders_marginality_report",
-    "ОтчетНатуральныеПоказателиПриемаИОтправкиПоЖД_TEU":
-        "natural_indicators_of_railway_reception_and_dispatch",
-    "СуммыСчетовПокупателям":
-        "accounts",
-    "СтавкиФрахта":
-        "freight_rates",
-    "МаржинальностиСделокНаОсновеАктов":
-        "marginality_orders_by_act_date",
-    "ДокументКоммерческоеПредложение":
-        "ruscon_products",
-    "СправочникМестоположения":
-        "reference_locations",
-    "ФактическаяЕмкостьТерминала":
-        "terminals_capacity",
-
-    # Данные по оценкам менеджеров
-    "ОценкиМенеджеров":
-        "manager_evaluation",
-
-    # Данные по справочнику контрагентов
-    "СправочникКонтрагентовДО":
-        "reference_counterparties",
-}
-
-LOG_TABLE: str = "rmq_log"
-
 # os.environ['TOKEN_TELEGRAM'] = '6557326533:AAGcYU6q0DFLJfjZMHTaDjLnf-PzRMzA-6M'
 # os.environ['XL_IDP_PATH_RABBITMQ'] = '/home/timur/sambashare/RabbitMQ'
 # os.environ['XL_IDP_ROOT_RABBITMQ'] = '/home/timur/PycharmWork/RabbitMQ'
@@ -85,10 +18,15 @@ def get_my_env_var(var_name: str) -> str:
         raise MissingEnvironmentVariable(f"{var_name} does not exist") from e
 
 
+DATE_FTM: str = "%d/%B/%Y %H:%M:%S"
+LOG_FORMAT: str = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
 LOG_DIR_NAME: str = f"{get_my_env_var('XL_IDP_ROOT_RABBITMQ')}/logging"
 LOG_FILE: str = f"{LOG_DIR_NAME}/processed_messages.json"
+LOG_TABLE: str = "rmq_log"
 with open(f"{get_my_env_var('XL_IDP_ROOT_RABBITMQ')}/queues_config.json", "r", encoding="utf-8") as file:
     QUEUES_AND_ROUTING_KEYS: dict = json.load(file)
+with open(f"{get_my_env_var('XL_IDP_ROOT_RABBITMQ')}/tables_config.json", "r", encoding="utf-8") as file:
+    TABLE_NAMES: dict = json.load(file)
 
 
 def get_file_handler(name: str) -> logging.FileHandler:
