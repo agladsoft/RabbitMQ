398be509fc3f869ba487fae5a02f287ec6fe9ff7
timur <agladsoft@gmail.com>
2025-04-01 09:39:30 +0300

    add: added DO queues

--- scripts/send2telegram.py
+++ scripts/send2telegram.py
@@ -1,3 +1,4 @@
+import socket
 import requests
 from scripts.__init__ import *
 
@@ -11,18 +12,18 @@ def handle_message(logs: dict, message: str, total_lines: int) -> str:
         messages.append(
             f"📥 Очередь: `{queue}`\n"
             f"📊 Обработанная таблица: \n`{processed_table}`\n"
-            f"🔢 Количество сообщений: *{count}*\n"
+            f"🔢 Количество сообщений: {count}\n"
         )
 
     if messages:
         message = "\n".join(messages)
-        message += f"\n📈 *Общее количество строк: {total_lines}*"
+        message += f"\n📈 Общее количество строк: {total_lines}"
 
     return message
 
 
 def send_message():
-    logger: get_logger = get_logger(os.path.basename(__file__).replace(".py", ""))
+    logger: get_logger = get_logger(str(os.path.basename(__file__).replace(".py", "")))
     logger.info("Send message to telegram")
     message: str = "Не было сообщений"
     if os.path.exists(LOG_FILE):
@@ -31,11 +32,16 @@ def send_message():
         if isinstance(logs, dict):
             total_lines: int = 0
             message = handle_message(logs, message, total_lines)
+
+    s: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('10.0.0.0', 0))
+    ip_server: str = s.getsockname()[0]
+
     params: dict = {
         "chat_id": f"{get_my_env_var('CHAT_ID')}/{get_my_env_var('TOPIC')}",
-        "text": f"\n{message}\n",
-        "parse_mode": "MarkdownV2",
-        "reply_to_message_id": get_my_env_var('MESSAGE_ID')
+        "text": f"Статистика обработки сообщений за день с RabbitMQ на сервере {ip_server}:\n"
+                f"<blockquote expandable>{message}</blockquote>",
+        "parse_mode": "HTML"
     }
     url: str = f"https://api.telegram.org/bot{get_my_env_var('TOKEN_TELEGRAM')}/sendMessage"
     response = requests.get(url, params=params)
